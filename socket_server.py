d9ede918ebcb5f2d10ad8bc835341a9486914604
Disassociative <devnull@localhost>
2011-05-19 00:22:40 +0400

    Optional port for socket and http

--- socket_server.py
+++ socket_server.py
@@ -5,15 +5,24 @@ from base.talker import Talker
 from base.test.ze_mapper import Mapper
 from ext.daemon import Daemon
 from http.http_socket import HttpSocket
+import logging
 import os
 import sys
 
 class SocketServer(Daemon):
 
-    def __init__(self, name, port, mapper):
+    def init_logging(self):
+        format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        level = logging.DEBUG
+
+        logging.basicConfig(format=format, level=level)
+
+    def __init__(self, name, port, mapper, http_port=None):
 
         self.port = port
         self.mapper = mapper
+        self.http_port = http_port
+
 
         def named(s):
             return s % name
@@ -27,6 +36,7 @@ class SocketServer(Daemon):
                         stdout=named_path('out_%s.log'))
 
     def run(self):
+        self.init_logging()
         Talker.port = self.port
         Common.mapper = self.mapper
 
@@ -34,12 +44,14 @@ class SocketServer(Daemon):
         self.talker = Talker()
         self.polisy_caster = PolicyCaster()
         self.polisy_caster.start()
-        self.http_socket = HttpSocket(self.mapper, self.port + 1)
+        if self.http_port:
+            self.http_socket = HttpSocket(self.mapper, self.http_port)
         self.talker.run()
 
     def close(self, *q):
         self.polisy_caster.close()
-        self.http_socket.stop()
+        if self.http_port:
+            self.http_socket.stop()
         self.talker.close()
         sys.exit(0)
 
