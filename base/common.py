0541461a10dcc87f0e440b2b3e193a7ae0a14a09
hg <devnull@localhost>
2011-05-19 12:13:01 +0400

    policy_xml changed

--- base/common.py
+++ base/common.py
@@ -5,7 +5,6 @@ class Common(object):
 
     mapper = None
     policy_xml = '''<?xml version="1.0"?>
-<!DOCTYPE cross-domain-policy SYSTEM "http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd">
 <cross-domain-policy>
-<allow-access-from domain="*" to-ports="*" secure="false" />
+  <allow-access-from domain="*" />
 </cross-domain-policy>'''
