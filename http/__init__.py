1525523abf9e4bd15ca8f78872c7c51c661e9a0b
hg <devnull@localhost>
2011-08-30 15:21:39 +0400

    web_only decorator added

---
+++ http/__init__.py
@@ -0,0 +1,16 @@
+# coding: utf8
+
+from http.http_socket import WebClient
+
+def web_only(f_exec):
+
+    def wrapper(object, *args, **kwargs):
+
+        if not isinstance(object.client, WebClient):
+            return object.msg.error('only for web request')
+
+        return f_exec(object, *args, **kwargs)
+
+    return wrapper
+
+
