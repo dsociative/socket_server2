d29efd814142bc5bce829715c15b246028090055
artem.prikhodin <admin@geektech.ru>
2012-07-28 15:55:02 +0400

    setup.py package init, all source moved to src

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='socket server2',
+      description='socket server for flash client',
+      author='dsociative',
+      author_email='admin@geektech.ru',
+      packages=['socket_server', 'socket_server.base',
+                'socket_server.ext', 'socket_server.http'],
+      package_dir={'socket_server': 'src'},
+     )
